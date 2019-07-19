b3493d150d46be5142e8e271f7b6aa1befb326ca
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-07-19 17:38:31 +0300

    init commit

---
+++ RainbowTabs/PurpleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PurpleViewController.swift
+//  RainbowTabs
+//
+//  Created by Владислав Павленко on 7/1/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class PurpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RainbowTabs/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  RainbowTabs
+//
+//  Created by Владислав Павленко on 7/1/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
